80e323b59092599d33d993ce1f4bff86fbf16075
Pragatibaghel31 <95239459+Pragatibaghel31@users.noreply.github.com>
2024-11-08 18:35:18 +0000

    container.config

--- Basics/Datatypes.js
+++ Basics/Datatypes.js
@@ -15,6 +15,53 @@
 console.log(typeof undefined);// undefined
 console.log(typeof null);// object
 
+// Datatype conversion cofusion
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let Value= Number(score)
+console.log(typeof Value);
+console.log(Value);
+
+let val=null
+console.log(typeof val);
+console.log(typeof (val));
+
+
+let val1=undefined
+console.log(typeof val1);
+console.log(typeof (val1));
+
+
+let val2=true
+console.log(typeof val2);
+console.log(typeof (val2));
+
+
+let val3="Pragati"
+console.log(typeof val3);
+console.log(typeof (val3));
+
+// "33"=>33
+// "33abc"=>NaN
+// true => 1; false =>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+// ""=>false
+//"Pragati" => true
+
+let somenum=33
+let stringnum=String(somenum)
+console.log(stringnum);
+console.log(typeof stringnum);
+
+
 
 
 
