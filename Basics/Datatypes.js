d2bd109a793f78cfde7dbe20228d6fe7ea65ed5b
Pragatibaghel31 <95239459+Pragatibaghel31@users.noreply.github.com>
2024-11-08 19:00:04 +0000

    container.config

--- Basics/Datatypes.js
+++ Basics/Datatypes.js
@@ -61,6 +61,24 @@ let stringnum=String(somenum)
 console.log(stringnum);
 console.log(typeof stringnum);
 
+// ************************ Operations **********************
+let num=3
+let negnum= -num
+console.log(negnum);
+
+console.log(+true);
+console.log(+"");
+
+console.log("2">1);
+console.log("02">1);// dont allow in typeScript
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);// the reason is that an equality check == and comparisons > < >= <= work differently
+//comparisons covert null to a number treating it as 0.
+// thats why (3)null >=0 is true and  (1) null>0 is false.
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
 
 
 
