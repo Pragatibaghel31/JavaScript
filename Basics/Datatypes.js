f3fa620f138c87bae5413fe3a5f044440ef608cd
Pragatibaghel31 <95239459+Pragatibaghel31@users.noreply.github.com>
2024-11-08 18:18:56 +0000

    container.config

---
+++ Basics/Datatypes.js
@@ -0,0 +1,20 @@
+"use Strict"// treat all JS Code as newer version
+
+//alert(3+3) // we are using node.js not browser
+
+
+// number => 2 to power 53
+// bigint
+// string =>
+// boolean => true/false
+// null => standalone value
+// undefined => if data is not yet filled it will show undefined as output
+// symbol => unique
+
+// object 
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+
+
+
+
